dee92743811601251457ea25c884053140f79ba4
Shivakant6387 <shivakantsingh900@gmail.com>
2024-01-22 01:42:22 +0530

    A to Z and PalindromeNumbers'

--- src/main/java/org/example/PalindromeNumbers.java
+++ src/main/java/org/example/PalindromeNumbers.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class PalindromeNumbers {
     public static void main(String[] args) {
-
+        PalindromeNumbers palindromeNumbers=new PalindromeNumbers();
+        int n=10;
+        System.out.println(palindromeNumbers.isPalindrome(n));
+    }
+    boolean isPalindrome(int n){
+        int rev=0;
+        int temp=n;
+        while(temp!=0){
+            int ld =temp%10;
+            rev=rev*10+ld;
+            temp=temp/10;
+        }
+        return (rev==n);
     }
 }
