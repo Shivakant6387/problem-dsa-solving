6c6dc62ab3379ea494638c6b71ae63f25d6675ea
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-19 21:02:18 +0530

    oops

---
+++ src/main/java/org/example/oops/Test.java
@@ -0,0 +1,9 @@
+package org.example.oops;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person=new Person();
+        person.setName("Shivakant Singh");
+        System.out.println(person.getName());
+    }
+}
