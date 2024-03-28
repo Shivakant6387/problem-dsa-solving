1db2122b8856cd9dd2d8bfa38a5cde34eeb26051
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-29 02:29:34 +0530

    other methed print n to 1 using recursion

--- src/main/java/org/example/recursion/PrintNTo1.java
+++ src/main/java/org/example/recursion/PrintNTo1.java
@@ -3,6 +3,7 @@ package org.example.recursion;
 public class PrintNTo1 {
     public static void main(String[] args) {
         printNToOne(10);
+        printNToOnes(5);
     }
 
     public static void printNToOne(int num) {
@@ -11,11 +12,11 @@ public class PrintNTo1 {
         System.out.println(num + " ");
         printNToOne(num - 1);
     }
-//
-//    public static int printNToOnes(int num) {
-//        if (num == 0)
-//            return 1;
-//        System.out.println(num + " ");
-//       return printNToOne(num - 1);
-//    }
+
+    public static int printNToOnes(int num) {
+        if (num == 0)
+            return 1;
+        System.out.println(num + " ");
+        return printNToOnes(num - 1);
+    }
 }
